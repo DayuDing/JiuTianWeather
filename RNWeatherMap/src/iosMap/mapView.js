bc008af41604eabfcc108fe8bb0d25d19a96621d
dingjinyu <dingjinyuit@163.com>
2018-08-06 16:11:35 +0800

    clear

--- RNWeatherMap/src/iosMap/mapView.js
+++
@@ -1,6 +0,0 @@
-// MapView.js
-
-import { requireNativeComponent } from 'react-native';
-
-// requireNativeComponent 自动把'RNTMap'解析为'RNTMapManager'
-export default requireNativeComponent('JYMapView', null);
