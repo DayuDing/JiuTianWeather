fbc1550bf0074404834d425d71dfa3e95ec5f243
dingjinyu <dingjinyuit@163.com>
2018-08-06 15:40:59 +0800

    new

---
+++ src/iosMap/mapView.js
@@ -0,0 +1,6 @@
+// MapView.js
+
+import { requireNativeComponent } from 'react-native';
+
+// requireNativeComponent 自动把'RNTMap'解析为'RNTMapManager'
+export default requireNativeComponent('JYMapView', null);
